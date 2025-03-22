f62e95a646424523a4742b36a2aac24803e8a25f
Gean Anjos <gean.miguelf@gmail.com>
2025-03-22 12:08:15 -0300

    feat: add the controller and first endpoint Create product

---
+++ src/SalesSystem.API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SalesSystem.Application.Commands.CreateProduct;
+using SalesSystem.Application.DTOs;
+
+namespace SalesSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductDto>> Create([FromBody] CreateProductCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDto>> GetById(int id)
+        {
+            return Ok();
+        }
+    }
+}
