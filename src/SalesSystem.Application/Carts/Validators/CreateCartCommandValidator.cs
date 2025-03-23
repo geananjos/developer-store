b201a32761d0fd3655cef70a6f9bea22be675802
Gean Anjos <gean.miguelf@gmail.com>
2025-03-23 11:48:55 -0300

    feat: add cart validators

---
+++ src/SalesSystem.Application/Carts/Validators/CreateCartCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using SalesSystem.Application.Carts.Commands.CreateCart;
+
+namespace SalesSystem.Application.Carts.Validators
+{
+    public class CreateCartCommandValidator : AbstractValidator<CreateCartCommand>
+    {
+        public CreateCartCommandValidator()
+        {
+            RuleFor(x => x.UserId)
+                .GreaterThan(0);
+
+            RuleFor(x => x.Date)
+                .NotEmpty();
+
+            RuleFor(x => x.Products)
+                .NotEmpty().WithMessage("O carrinho precisa conter ao menos um produto.");
+
+            RuleForEach(x => x.Products).ChildRules(product =>
+            {
+                product.RuleFor(p => p.ProductId)
+                    .GreaterThan(0).WithMessage("ID do produto deve ser maior que zero.");
+
+                product.RuleFor(p => p.Quantity)
+                    .GreaterThan(0).WithMessage("Quantidade deve ser maior que zero.");
+            });
+        }
+    }
+}
---
+++ src/SalesSystem.Application/Carts/Validators/UpdateCartCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using SalesSystem.Application.Carts.Commands.UpdateCart;
+
+namespace SalesSystem.Application.Carts.Validators
+{
+    public class UpdateCartCommandValidator : AbstractValidator<UpdateCartCommand>
+    {
+        public UpdateCartCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0);
+
+            RuleFor(x => x.UserId)
+                .GreaterThan(0);
+
+            RuleFor(x => x.Date)
+                .NotEmpty();
+
+            RuleFor(x => x.Products)
+                .NotEmpty().WithMessage("O carrinho precisa conter ao menos um produto.");
+
+            RuleForEach(x => x.Products).ChildRules(product =>
+            {
+                product.RuleFor(p => p.ProductId)
+                    .GreaterThan(0);
+
+                product.RuleFor(p => p.Quantity)
+                    .GreaterThan(0);
+            });
+        }
+    }
+}
