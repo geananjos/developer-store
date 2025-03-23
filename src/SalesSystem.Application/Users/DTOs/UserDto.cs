c0b0a2ac60cbe6dce00205c7442297dc1698cd04
Gean Anjos <gean.miguelf@gmail.com>
2025-03-22 21:06:14 -0300

    feat: add user mapping profile

---
+++ src/SalesSystem.Application/Users/DTOs/UserDto.cs
@@ -0,0 +1,39 @@
+﻿namespace SalesSystem.Application.Users.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+
+        public NameDto Name { get; set; } = null!;
+        public AddressDto Address { get; set; } = null!;
+
+        public string Phone { get; set; } = null!;
+        public string Status { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+
+    public class NameDto
+    {
+        public string Firstname { get; set; } = null!;
+        public string Lastname { get; set; } = null!;
+    }
+
+    public class AddressDto
+    {
+        public string City { get; set; } = null!;
+        public string Street { get; set; } = null!;
+        public int Number { get; set; }
+        public string Zipcode { get; set; } = null!;
+        public GeolocationDto Geolocation { get; set; } = null!;
+    }
+
+    public class GeolocationDto
+    {
+        public string Lat { get; set; } = null!;
+        public string Long { get; set; } = null!;
+    }
+
+}
---
+++ src/SalesSystem.Application/Users/Mapping/UserMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using SalesSystem.Application.Users.DTOs;
+using SalesSystem.Domain.Users;
+using SalesSystem.Domain.Users.ValueObjects;
+
+namespace SalesSystem.Application.Users.Mapping
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<User, UserDto>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()))
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role.ToString()));
+
+            CreateMap<Name, NameDto>();
+            CreateMap<Address, AddressDto>();
+            CreateMap<Geolocation, GeolocationDto>();
+        }
+    }
+}
